96d8046cc81c849ccabb0e847ae14a6225aaf581
LiHongTai <631396529@qq.com>
2018-05-29 19:35:18 +0800

    first commit

--- src/main/java/com/roger/data/controller/DeptController.java
+++ src/main/java/com/roger/data/controller/DeptController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
-
+//注解@RestController是 @Controller 和 @ResponseBody 注解的结合体
 @RestController
 @RequestMapping(value = "/dept")
 public class DeptController {
@@ -25,4 +25,31 @@ public class DeptController {
         return dept;
     }
 
+
+    @GetMapping(value = "/create")
+    public Dept create(){
+        Dept dept = new Dept();
+        dept.setDName("研发部");
+        dept.setLocation("ShangHai");
+
+        deptRepository.save(dept);
+        return dept;
+    }
+
+    @GetMapping(value = "delete/{id}")
+    public void deleteById(@PathVariable("id") Integer id){
+        deptRepository.deleteById(id);
+    }
+
+    @GetMapping(value = "update/{id}")
+    public Dept update(@PathVariable("id") Integer id){
+        Dept dept = null;
+        Optional<Dept> optionalDept = deptRepository.findById(id);
+        if(optionalDept.isPresent()){
+            dept = optionalDept.get();
+            dept.setLocation("AnHui");
+            deptRepository.save(dept);
+        }
+        return dept;
+    }
 }
--- src/main/java/com/roger/data/repository/DeptRepository.java
+++ src/main/java/com/roger/data/repository/DeptRepository.java
@@ -5,4 +5,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface DeptRepository extends JpaRepository<Dept,Integer> {
 
+    //JPQL 持久化查询语句 -- 类SQL语句
+    //从SQL语句转换成JPQL语句，只需要注意一下几点
+    //1. 大多数情况下，* 替换成 别名
+    //2. 所有出现表名的地方替换成实体类名
+    //3. 所有列名替换成属性名
+    //4. 参数传递 --> :dName 配合注解 @Param 使用
 }
